067d5e7075763540ab54dff560f4227dd2899423
Lt-Abstergo <azizmorshedkhan@gmail.com>
2021-12-04 07:24:17 -0500

    1

--- DatabaseHandler.py
+++ DatabaseHandler.py
@@ -1,14 +1,59 @@
 import sqlite3
 
-conn = sqlite3.connect("pass_man.db")
-cursor = conn.cursor()
 
-cursor.execute("""CREATE TABLE id(
-                    name TEXT,
-                    url TEXT,
-                    username TEXT,
-                    password TEXT
-                    )""")
-print("database successfully created")
-conn.commit()
-conn.close()
+def create_table(dbname):
+    conn = sqlite3.connect(dbname + ".db")
+    cursor = conn.cursor()
+    cursor.execute("""CREATE TABLE id(
+                        name TEXT,
+                        url TEXT,
+                        username TEXT,
+                        password TEXT
+                        )""")
+    print("database successfully created")
+    conn.commit()
+    conn.close()
+
+
+def add_one(dbname, name, url, username, password):
+    conn = sqlite3.connect(dbname + ".db")
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO id VALUES (?,?,?,?)", (name, url, username, password))
+    conn.commit()
+    conn.close()
+
+
+def add_multiple(dbname, list):
+    conn = sqlite3.connect(dbname + ".db")
+    cursor = conn.cursor()
+    cursor.executemany("INSERT INTO id VALUES (?,?,?,?)", list)
+    conn.commit()
+    conn.close()
+
+
+def show_all(dbname):
+    conn = sqlite3.connect(dbname + ".db")
+    cursor = conn.cursor()
+    cursor.execute("SELECT rowid, * from id")
+    items = cursor.fetchall()
+    for item in items:
+        print(item)
+    conn.commit()
+    conn.close()
+
+
+def delete_one(rowid):
+    conn = sqlite3.connect(dbname + ".db")
+    cursor = conn.cursor()
+    cursor.execute("DELETE FROM id where rowid=(?)", str(rowid))
+    conn.commit()
+    conn.close()
+
+
+def get_row_id(dbname, )
+
+
+dbname = "pass_man"
+show_all(dbname)
+delete_one(1)
+show_all(dbname)
